bf120c0cb6f55c0dfa8adf823d199632aafd8b7f
min <wjdals2803@naver.com>
2025-02-18 14:30:39 +0900

    0218 수업내용 전달

---
+++ javac테스트/HelloStart.java
@@ -0,0 +1,9 @@
+public class HelloStart{
+     public static void main(String [] arr){
+
+     System.out.println("**Java Start!!**");
+     System.out.print("**æ»≥Á«œººø‰**");
+     System.out.print("**¡§πŒ¥‘**");
+  }
+
+}
