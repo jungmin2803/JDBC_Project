43f1250e776c134bb025659e203b90a1dcbc9f72
jungmin2803 <wjdals2803@naver.com>
2025-03-18 17:49:38 +0900

    0318 수업내용

---
+++ JavaProject/SistJavaProject/src/day0313/Ex02.java
@@ -0,0 +1,28 @@
+package day0313;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		
+		
+			String n;
+			Random rd=new Random();
+			for(int i=1; i<3 ; i++)
+			{
+				System.out.println("엔터를 누르면 난수 20~45 사이 난수 2개 발생 q(종료)");
+				int r=(sc.nextInt(20)+1);
+				
+			}
+			
+			
+		
+		
+	}
+
+}
